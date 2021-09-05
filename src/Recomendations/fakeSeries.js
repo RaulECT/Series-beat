22bbfc3cd6368aa3dd6391782a771bfa555f0715
RaulECT <eduardoct120@gmail.com>
2021-09-04 20:13:08 -0500

    fake data created

---
+++ src/Recomendations/fakeSeries.js
@@ -0,0 +1,82 @@
+export const fakeSeries = [
+  {
+    id: 1,
+    name: 'Love Alarm',
+    imageUrl: 'https://static.wikia.nocookie.net/drama/images/8/83/Love_Alarm-Netflix-2019-01.jpg/revision/latest?cb=20190729035203&path-prefix=es'
+  },
+  {
+    id: 2,
+    name: 'Love Alarm',
+    imageUrl: 'https://static.wikia.nocookie.net/drama/images/1/1e/Itaewon_Class-jTBC-2020-06.jpg/revision/latest?cb=20200117082237&path-prefix=es'
+  },
+  {
+    id: 3,
+    name: 'Love Alarm',
+    imageUrl: 'https://static.wikia.nocookie.net/drama/images/8/83/Love_Alarm-Netflix-2019-01.jpg/revision/latest?cb=20190729035203&path-prefix=es'
+  },
+  {
+    id: 4,
+    name: 'Love Alarm',
+    imageUrl: 'https://static.wikia.nocookie.net/drama/images/1/1e/Itaewon_Class-jTBC-2020-06.jpg/revision/latest?cb=20200117082237&path-prefix=es'
+  },
+  {
+    id: 5,
+    name: 'Love Alarm',
+    imageUrl: 'https://static.wikia.nocookie.net/drama/images/8/83/Love_Alarm-Netflix-2019-01.jpg/revision/latest?cb=20190729035203&path-prefix=es'
+  },
+  {
+    id: 6,
+    name: 'Love Alarm',
+    imageUrl: 'https://static.wikia.nocookie.net/drama/images/1/1e/Itaewon_Class-jTBC-2020-06.jpg/revision/latest?cb=20200117082237&path-prefix=es'
+  },
+  {
+    id: 7,
+    name: 'Love Alarm',
+    imageUrl: 'https://static.wikia.nocookie.net/drama/images/8/83/Love_Alarm-Netflix-2019-01.jpg/revision/latest?cb=20190729035203&path-prefix=es'
+  },
+  {
+    id: 8,
+    name: 'Love Alarm',
+    imageUrl: 'https://static.wikia.nocookie.net/drama/images/1/1e/Itaewon_Class-jTBC-2020-06.jpg/revision/latest?cb=20200117082237&path-prefix=es'
+  },
+  {
+    id: 9,
+    name: 'Love Alarm',
+    imageUrl: 'https://static.wikia.nocookie.net/drama/images/8/83/Love_Alarm-Netflix-2019-01.jpg/revision/latest?cb=20190729035203&path-prefix=es'
+  },
+  {
+    id: 10,
+    name: 'Love Alarm',
+    imageUrl: 'https://static.wikia.nocookie.net/drama/images/1/1e/Itaewon_Class-jTBC-2020-06.jpg/revision/latest?cb=20200117082237&path-prefix=es'
+  },
+  {
+    id: 11,
+    name: 'Love Alarm',
+    imageUrl: 'https://static.wikia.nocookie.net/drama/images/8/83/Love_Alarm-Netflix-2019-01.jpg/revision/latest?cb=20190729035203&path-prefix=es'
+  },
+  {
+    id: 12,
+    name: 'Love Alarm',
+    imageUrl: 'https://static.wikia.nocookie.net/drama/images/1/1e/Itaewon_Class-jTBC-2020-06.jpg/revision/latest?cb=20200117082237&path-prefix=es'
+  },
+  {
+    id: 13,
+    name: 'Love Alarm',
+    imageUrl: 'https://static.wikia.nocookie.net/drama/images/8/83/Love_Alarm-Netflix-2019-01.jpg/revision/latest?cb=20190729035203&path-prefix=es'
+  },
+  {
+    id: 14,
+    name: 'Love Alarm',
+    imageUrl: 'https://static.wikia.nocookie.net/drama/images/1/1e/Itaewon_Class-jTBC-2020-06.jpg/revision/latest?cb=20200117082237&path-prefix=es'
+  },
+  {
+    id: 15,
+    name: 'Love Alarm',
+    imageUrl: 'https://static.wikia.nocookie.net/drama/images/8/83/Love_Alarm-Netflix-2019-01.jpg/revision/latest?cb=20190729035203&path-prefix=es'
+  },
+  {
+    id: 16,
+    name: 'Love Alarm',
+    imageUrl: 'https://static.wikia.nocookie.net/drama/images/1/1e/Itaewon_Class-jTBC-2020-06.jpg/revision/latest?cb=20200117082237&path-prefix=es'
+  },
+];
