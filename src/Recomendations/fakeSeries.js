db2df3e32104a4e0927766aa6fb16fb47f888463
RaulECT <eduardoct120@gmail.com>
2021-09-05 14:39:23 -0500

    data updated

--- src/Recomendations/fakeSeries.js
+++ src/Recomendations/fakeSeries.js
@@ -1,16 +1,10 @@
+import data from '../data.json';
 const fakeSeries = [];
 
 for (let index = 0; index < 60; index++) {
-  fakeSeries.push({
-    id: index + 1,
-    name: 'Love Alarm',
-    imageUrl: 'https://cde.laprensa.e3.pe/ima/0/0/2/3/0/230205.jpg'
-  });
-  fakeSeries.push({
-    id: index + 2,
-    name: 'Itaewon Class',
-    imageUrl: 'https://i0.wp.com/www.cupcakesharmony.com/wp-content/uploads/2020/05/e1d33c1e0ea089049c853b7a2a8966d7.jpg'
-  });
+  const randomItem = data.serires[Math.floor(Math.random() * (data.serires.length - 1))];
+
+  fakeSeries.push(randomItem);
 }
 
 export default fakeSeries;
